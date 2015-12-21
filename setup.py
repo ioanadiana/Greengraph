23774c0cc2067aff973142b838ee53c20e06a7d6
Ioana Oprea <ioana.diana.oprea@gmail.com>
2015-12-21 18:05:11 +0000

    Added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Greengraph",
+    version = "1.0",
+    packages = find_packages(exclude=['*test']),
+    scripts = ['scripts/greengraph'],
+    install_requires = ['argparse','geopy','numpy','matplotlib']
+)
