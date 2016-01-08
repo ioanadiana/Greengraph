b432753ce5e86824e0306dd942e236a8b4f30faa
ioanadiana <ioana.diana.oprea@gmail.com>
2016-01-08 09:24:36 +0000

    Updated command.py

--- greengraph/command.py
+++ greengraph/command.py
@@ -4,7 +4,8 @@ from graph import Greengraph
 
 def process():
     parser = ArgumentParser(description = "Generate graph of proportion of green pixel in a series of satellite images between two points:")
-    parser.add_argument('--fromS', '-f')
+    
+    parser.add_argument('--from', '-f', dest = 'fromCity')
     parser.add_argument('--to')
     parser.add_argument('--steps')
     parser.add_argument('--out')
